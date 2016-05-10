5f55194fcd0a97c822db72de7c574d6385e1d26b
kanxiaojie <kanhappyday2008@163.com>
2016-05-10 23:06:43 +0800

    login page

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,60 +1,32 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <div class="row">
-        <div class="col-md-8 col-md-offset-2">
-            <div class="panel panel-default">
-                <div class="panel-heading">Login</div>
+
+    <div class="row" style="background: url('/images/photos/QQ截图20160510224312.png');height: 490px;margin: 0px;">
+        <div class="col-md-4 col-md-offset-4" style="text-align: center">
+            <div class="h3" style="color: #3673b6;">文章展示</div>
+
+            <div class="panel panel-default" style="background: #EAEAEA;padding-top: 25px;padding-bottom:5px;padding-left: 50px;padding-right: 50px;">
                 <div class="panel-body">
                     <form class="form-horizontal" role="form" method="POST" action="{{ url('/login') }}">
-                        {!! csrf_field() !!}
+                        {{ csrf_field() }}
+                        <input type="hidden" name="_token" value="{{ csrf_token() }}">
 
-                        <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
-                            <label class="col-md-4 control-label">E-Mail Address</label>
-
-                            <div class="col-md-6">
-                                <input type="email" class="form-control" name="email" value="{{ old('email') }}">
-
-                                @if ($errors->has('email'))
-                                    <span class="help-block">
-                                        <strong>{{ $errors->first('email') }}</strong>
-                                    </span>
-                                @endif
-                            </div>
-                        </div>
-
-                        <div class="form-group{{ $errors->has('password') ? ' has-error' : '' }}">
-                            <label class="col-md-4 control-label">Password</label>
-
-                            <div class="col-md-6">
-                                <input type="password" class="form-control" name="password">
-
-                                @if ($errors->has('password'))
-                                    <span class="help-block">
-                                        <strong>{{ $errors->first('password') }}</strong>
-                                    </span>
-                                @endif
-                            </div>
+                        <div class="form-group">
+                            <input class="form-control" name="email" placeholder="请输入用户邮箱" value="">
                         </div>
 
                         <div class="form-group">
-                            <div class="col-md-6 col-md-offset-4">
-                                <div class="checkbox">
-                                    <label>
-                                        <input type="checkbox" name="remember"> Remember Me
-                                    </label>
-                                </div>
-                            </div>
+                            <input type="password" class="form-control" name="password" placeholder="请输入密码">
+
                         </div>
 
-                        <div class="form-group">
-                            <div class="col-md-6 col-md-offset-4">
+                        <div class="form-group" style="padding-top: 10px;">
+                            <div class="text-center">
                                 <button type="submit" class="btn btn-primary">
-                                    <i class="fa fa-btn fa-sign-in"></i>Login
+                                    登录
                                 </button>
 
-                                <a class="btn btn-link" href="{{ url('/password/reset') }}">Forgot Your Password?</a>
                             </div>
                         </div>
                     </form>
@@ -62,5 +34,68 @@
             </div>
         </div>
     </div>
-</div>
+{{--<div class="container"  style="height: 100%;width: 100%;background:url('images/photos/QQ截图20160509225241.png') no-repeat;background-position:0px 70px 0px 70px;">--}}
+    {{--<div class="row">--}}
+        {{--<div class="col-md-8 col-md-offset-2">--}}
+            {{--<div class="panel panel-default">--}}
+                {{--<div class="panel-heading">Login</div>--}}
+                {{--<div class="panel-body">--}}
+                    {{--<form class="form-horizontal" role="form" method="POST" action="{{ url('/login') }}">--}}
+                        {{--{!! csrf_field() !!}--}}
+
+                        {{--<div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">--}}
+                            {{--<label class="col-md-4 control-label">E-Mail Address</label>--}}
+
+                            {{--<div class="col-md-6">--}}
+                                {{--<input type="email" class="form-control" name="email" value="{{ old('email') }}">--}}
+
+                                {{--@if ($errors->has('email'))--}}
+                                    {{--<span class="help-block">--}}
+                                        {{--<strong>{{ $errors->first('email') }}</strong>--}}
+                                    {{--</span>--}}
+                                {{--@endif--}}
+                            {{--</div>--}}
+                        {{--</div>--}}
+
+                        {{--<div class="form-group{{ $errors->has('password') ? ' has-error' : '' }}">--}}
+                            {{--<label class="col-md-4 control-label">Password</label>--}}
+
+                            {{--<div class="col-md-6">--}}
+                                {{--<input type="password" class="form-control" name="password">--}}
+
+                                {{--@if ($errors->has('password'))--}}
+                                    {{--<span class="help-block">--}}
+                                        {{--<strong>{{ $errors->first('password') }}</strong>--}}
+                                    {{--</span>--}}
+                                {{--@endif--}}
+                            {{--</div>--}}
+                        {{--</div>--}}
+
+                        {{--<div class="form-group">--}}
+                            {{--<div class="col-md-6 col-md-offset-4">--}}
+                                {{--<div class="checkbox">--}}
+                                    {{--<label>--}}
+                                        {{--<input type="checkbox" name="remember"> Remember Me--}}
+                                    {{--</label>--}}
+                                {{--</div>--}}
+                            {{--</div>--}}
+                        {{--</div>--}}
+
+                        {{--<div class="form-group">--}}
+                            {{--<div class="col-md-6 col-md-offset-4">--}}
+                                {{--<button type="submit" class="btn btn-primary">--}}
+                                    {{--<i class="fa fa-btn fa-sign-in"></i>Login--}}
+                                {{--</button>--}}
+
+                                {{--<a class="btn btn-link" href="{{ url('/password/reset') }}">Forgot Your Password?</a>--}}
+                            {{--</div>--}}
+                        {{--</div>--}}
+                    {{--</form>--}}
+                {{--</div>--}}
+            {{--</div>--}}
+        {{--</div>--}}
+    {{--</div>--}}
+{{--</div>--}}
+
+<img src="/images/photos/QQ截图20160510223038.png" style="border-radius: 0px;margin-bottom: 0px;" alt>
 @endsection
