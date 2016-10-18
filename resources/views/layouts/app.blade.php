dca74fbe2b0e3b187a19a712597cb3cbd3ffaf2f
kanxiaojie <kanhappyday2008@163.com>
2016-10-18 18:16:39 +0800

    replace source in login page

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -29,57 +29,13 @@
 <body style="padding: 0;">
     <img src="/images/QQ截图20160510223004.png" style="border-radius: 0px;margin-left: 50px;margin-top: 30px;" class="shadow" alt="logo">
     <img src="/images/QQ截图20160510223543.png" style="border-radius: 0px;float:right;width: 300px;height: 250px;">
-    {{--<nav class="navbar navbar-default navbar-static-top">--}}
-        {{--<div class="container">--}}
-            {{--<div class="navbar-header">--}}
 
-                {{--<!-- Collapsed Hamburger -->--}}
-                {{--<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#app-navbar-collapse">--}}
-                    {{--<span class="sr-only">Toggle Navigation</span>--}}
-                    {{--<span class="icon-bar"></span>--}}
-                    {{--<span class="icon-bar"></span>--}}
-                    {{--<span class="icon-bar"></span>--}}
-                {{--</button>--}}
-
-                {{--<!-- Branding Image -->--}}
-                {{--<a class="navbar-brand" href="{{ url('/') }}">--}}
-                    {{--LaravelForum--}}
-                {{--</a>--}}
-            {{--</div>--}}
-
-            {{--<div class="collapse navbar-collapse" id="app-navbar-collapse">--}}
-                {{--<!-- Left Side Of Navbar -->--}}
-                {{--<ul class="nav navbar-nav">--}}
-                    {{--<li><a href="{{ url('/') }}">Home</a></li>--}}
-                {{--</ul>--}}
-
-                {{--<!-- Right Side Of Navbar -->--}}
-                {{--<ul class="nav navbar-nav navbar-right">--}}
-                    {{--<!-- Authentication Links -->--}}
-                    {{--@if (Auth::guest())--}}
-                        {{--<li><a href="{{ url('/login') }}">Login</a></li>--}}
-                        {{--<li><a href="{{ url('/register') }}">Register</a></li>--}}
-                    {{--@else--}}
-                        {{--<li class="dropdown">--}}
-                            {{--<a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">--}}
-                                {{--{{ Auth::user()->name }} <span class="caret"></span>--}}
-                            {{--</a>--}}
-
-                            {{--<ul class="dropdown-menu" role="menu">--}}
-                                {{--<li><a href="{{ url('/logout') }}"><i class="fa fa-btn fa-sign-out"></i>Logout</a></li>--}}
-                            {{--</ul>--}}
-                        {{--</li>--}}
-                    {{--@endif--}}
-                {{--</ul>--}}
-            {{--</div>--}}
-        {{--</div>--}}
-    {{--</nav>--}}
 
     @yield('content')
 
     <!-- JavaScripts -->
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
+    <script src="/js/jquery-2.2.2.js"></script>
+    <script src="/js/bootstrap.min.js"></script>
     {{-- <script src="{{ elixir('js/app.js') }}"></script> --}}
 </body>
 </html>
