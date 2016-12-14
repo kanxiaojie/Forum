67bf2bd2aec72c70f2609e65a2bda4596d2119cd
kanxiaojie <kanhappyday2008@163.com>
2016-12-14 17:56:06 +0800

    erweima

--- resources/views/posts/QrCode.blade.php
+++ resources/views/posts/QrCode.blade.php
@@ -2,6 +2,6 @@
 
 @section('content')
     <div class="container">
-        {!! QrCode::size(500)->color(20,157,210)->generate('http://'.$_SERVER['HTTP_HOST'].'/posts'.$id); !!}
+        {!! QrCode::size(500)->color(20,157,210)->generate('http://'.$_SERVER['HTTP_HOST'].'/posts/'.$id); !!}
     </div>
 @stop
