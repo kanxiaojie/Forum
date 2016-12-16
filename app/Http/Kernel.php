07800769098ab86be88d40d8983345392a79079d
kanxiaojie <kanhappyday2008@163.com>
2016-12-16 14:53:20 +0800

    limits IP

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -15,8 +15,8 @@ class Kernel extends HttpKernel
      * @var array
      */
     protected $middleware = [
-//        \Illuminate\Foundation\Http\Middleware\CheckForMaintenanceMode::class,
-        \App\Http\Middleware\CheckForMaintenanceMode::class,
+        \Illuminate\Foundation\Http\Middleware\CheckForMaintenanceMode::class,
+//        \App\Http\Middleware\CheckForMaintenanceMode::class,
         \App\Http\Middleware\EncryptCookies::class,
         \Illuminate\Cookie\Middleware\AddQueuedCookiesToResponse::class,
         \Illuminate\Session\Middleware\StartSession::class,
@@ -56,6 +56,5 @@ class Kernel extends HttpKernel
         'guest' => \App\Http\Middleware\RedirectIfAuthenticated::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'language' => \App\Http\Middleware\Language::class,
-        'check' => \App\Http\Middleware\CheckForMaintenanceMode::class,
     ];
 }
